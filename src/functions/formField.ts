c58b74ccb2e61c36e3c7020404e45eea75548783
Yoshihiko Takeuchi <yoshihiko-takeuchi@cybozu.co.jp>
2024-03-24 01:45:32 +0900

    fix: import type

--- src/functions/formField.ts
+++ src/functions/formField.ts
@@ -1,66 +1,48 @@
-import {
-  RecordNumber,
-  Creator,
-  CreatedTime,
-  Modifier,
-  UpdatedTime,
-  SingleLineText,
-  MultiLineText,
-  RichText,
-  Number,
-  Calc,
-  CheckBox,
-  RadioButton,
-  MultiSelect,
-  Dropdown,
-  UserSelect,
-  OrganizationSelect,
-  GroupSelect,
-  Date,
-  Time,
-  DateTime,
-  Link,
-  File,
-  Lookup,
-  Subtable,
-  Group,
-  ReferenceTable,
-  Category,
-  Status,
-  StatusAssignee,
-  InSubtable,
-  OneOf,
-} from "@kintone/rest-api-client/lib/src/KintoneFields/exportTypes/property";
+import { KintoneFormFieldProperty } from "@kintone/rest-api-client";
 
-const isRecordNumber = (field: OneOf): field is RecordNumber => field.type === "RECORD_NUMBER";
-const isCreator = (field: OneOf): field is Creator => field.type === "CREATOR";
-const isCreatedTime = (field: OneOf): field is CreatedTime => field.type === "CREATED_TIME";
-const isModifier = (field: OneOf): field is Modifier => field.type === "MODIFIER";
-const isUpdatedTime = (field: OneOf): field is UpdatedTime => field.type === "UPDATED_TIME";
-const isSingleLineText = (field: OneOf): field is SingleLineText => field.type === "SINGLE_LINE_TEXT";
-const isMultiLineText = (field: OneOf): field is MultiLineText => field.type === "MULTI_LINE_TEXT";
-const isRichText = (field: OneOf): field is RichText => field.type === "RICH_TEXT";
-const isNumber = (field: OneOf): field is Number => field.type === "NUMBER";
-const isCalc = (field: OneOf): field is Calc => field.type === "CALC";
-const isCheckBox = (field: OneOf): field is CheckBox => field.type === "CHECK_BOX";
-const isRadioButton = (field: OneOf): field is RadioButton => field.type === "RADIO_BUTTON";
-const isMultiSelect = (field: OneOf): field is MultiSelect => field.type === "MULTI_SELECT";
-const isDropDown = (field: OneOf): field is Dropdown => field.type === "DROP_DOWN";
-const isUserSelect = (field: OneOf): field is UserSelect => field.type === "USER_SELECT";
-const isOrganizationSelect = (field: OneOf): field is OrganizationSelect => field.type === "ORGANIZATION_SELECT";
-const isGroupSelect = (field: OneOf): field is GroupSelect => field.type === "GROUP_SELECT";
-const isDate = (field: OneOf): field is Date => field.type === "DATE";
-const isTime = (field: OneOf): field is Time => field.type === "TIME";
-const isDatetime = (field: OneOf): field is DateTime => field.type === "DATETIME";
-const isLink = (field: OneOf): field is Link => field.type === "LINK";
-const isFile = (field: OneOf): field is File => field.type === "FILE";
-const isLookup = (field: OneOf): field is Lookup => (field.type === "SINGLE_LINE_TEXT" || field.type === "NUMBER") && "lookup" in field;
-const isSubtable = (field: OneOf): field is Subtable<{ [fieldCode: string]: InSubtable }> => field.type === "SUBTABLE";
-const isGroup = (field: OneOf): field is Group => field.type === "GROUP";
-const isReferenceTable = (field: OneOf): field is ReferenceTable => field.type === "REFERENCE_TABLE";
-const isCategory = (field: OneOf): field is Category => field.type === "CATEGORY";
-const isStatus = (field: OneOf): field is Status => field.type === "STATUS";
-const isStatusAssignee = (field: OneOf): field is StatusAssignee => field.type === "STATUS_ASSIGNEE";
+const isRecordNumber = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.RecordNumber =>
+  field.type === "RECORD_NUMBER";
+const isCreator = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.Creator => field.type === "CREATOR";
+const isCreatedTime = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.CreatedTime =>
+  field.type === "CREATED_TIME";
+const isModifier = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.Modifier => field.type === "MODIFIER";
+const isUpdatedTime = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.UpdatedTime =>
+  field.type === "UPDATED_TIME";
+const isSingleLineText = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.SingleLineText =>
+  field.type === "SINGLE_LINE_TEXT";
+const isMultiLineText = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.MultiLineText =>
+  field.type === "MULTI_LINE_TEXT";
+const isRichText = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.RichText => field.type === "RICH_TEXT";
+const isNumber = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.Number => field.type === "NUMBER";
+const isCalc = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.Calc => field.type === "CALC";
+const isCheckBox = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.CheckBox => field.type === "CHECK_BOX";
+const isRadioButton = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.RadioButton =>
+  field.type === "RADIO_BUTTON";
+const isMultiSelect = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.MultiSelect =>
+  field.type === "MULTI_SELECT";
+const isDropDown = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.Dropdown => field.type === "DROP_DOWN";
+const isUserSelect = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.UserSelect => field.type === "USER_SELECT";
+const isOrganizationSelect = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.OrganizationSelect =>
+  field.type === "ORGANIZATION_SELECT";
+const isGroupSelect = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.GroupSelect =>
+  field.type === "GROUP_SELECT";
+const isDate = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.Date => field.type === "DATE";
+const isTime = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.Time => field.type === "TIME";
+const isDatetime = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.DateTime => field.type === "DATETIME";
+const isLink = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.Link => field.type === "LINK";
+const isFile = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.File => field.type === "FILE";
+const isLookup = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.Lookup =>
+  (field.type === "SINGLE_LINE_TEXT" || field.type === "NUMBER") && "lookup" in field;
+const isSubtable = (
+  field: KintoneFormFieldProperty.OneOf,
+): field is KintoneFormFieldProperty.Subtable<{ [fieldCode: string]: KintoneFormFieldProperty.InSubtable }> => field.type === "SUBTABLE";
+const isGroup = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.Group => field.type === "GROUP";
+const isReferenceTable = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.ReferenceTable =>
+  field.type === "REFERENCE_TABLE";
+const isCategory = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.Category => field.type === "CATEGORY";
+const isStatus = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.Status => field.type === "STATUS";
+const isStatusAssignee = (field: KintoneFormFieldProperty.OneOf): field is KintoneFormFieldProperty.StatusAssignee =>
+  field.type === "STATUS_ASSIGNEE";
 
 export {
   isRecordNumber,
