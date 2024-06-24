3d169ed54c14f8b7b280cd6d35a2de75facc8c05
Yoshihiko Takeuchi <yoshihiko-takeuchi@cybozu.co.jp>
2024-06-24 19:58:30 +0900

    feat: fix type

--- vite-test.config.ts
+++ vite-test.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		dir: "test/vitest",
+		dir: "src/test/vitest",
 		include: ["**/*.test.ts"],
 		typecheck: { enabled: true },
 	},
